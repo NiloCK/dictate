86b0aa1633f4b013347ee7d134316dfb50b8c3e9
NiloCK <colin@mrkennedy.ca>
2026-01-07 11:24:04 -0400

    consume args

--- src/dictation.sh
+++ src/dictation.sh
@@ -1,6 +1,5 @@
-
 #!/bin/bash
 # dictation.sh
 
 # Activate virtual environment and run the client
-/opt/dictation_venv/bin/python /usr/local/bin/dictation_client.py
+/opt/dictation_venv/bin/python /usr/local/bin/dictation_client.py "$@"
